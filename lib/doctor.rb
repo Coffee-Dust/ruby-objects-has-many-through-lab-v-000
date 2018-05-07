1bb0562c1b2521bedfddd31cbf4d4f0d5877e4ad
Jonathan Dust <d24dust@hotmail.com>
2018-05-07 19:30:05 +0000

    automatically backed up by learn

---
+++ lib/doctor.rb
@@ -0,0 +1,13 @@
+class Doctor
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+    @appointments = []
+  end
+
+  def add_appointment(appt)
+    appt.doctor
+    @appointments << appt if appt is_a? Appointment
+  end
+end
